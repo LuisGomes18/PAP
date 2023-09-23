f1b93befcd9c76702eddda6445f79dc5b91b3b85
LuisGomes18 <luiscgomes0@gmail.com>
2023-09-23 17:22:10 +0100

    Adição do ficheiro di server e da pagina

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('\033[33m' + 'Criação finalizada\n'+ '\033[0;0m')
+'''
+flask: Modulo de que permite criar um servidor web
+'''
+from flask import Flask # pylint: disable=import-error
+from flask import render_template # pylint: disable=import-error
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    '''Função que retorna a pagina index.html'''
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
