7be5066823557a9e32282309fe87b2622293da88
LuisGomes18 <luiscgomes0@gmail.com>
2023-10-26 14:54:37 +0100

    Adição test da cpu e modificacao do  teste de ram

--- test/ram_check.py
+++ test/ram_check.py
@@ -15,11 +15,13 @@ try:
         print('Server não pode ser rodado')
     else:
         print('Rodando o server')
+        """"
         if os.name == 'posix':  # Verifica se está em um sistema Unix (Linux, macOS)
             activar_script = os.path.join(venv_path, "bin", "activate")
             subprocess.run(f"source {activar_script} && python /home/luis/Desktop/PAP/PAP_Com_Server/app.py", shell=True, text=True, check=True, executable="/bin/bash")
         elif os.name == 'nt':  # Verifica se está no Windows
             activate_script = os.path.join(venv_path, "Scripts", "activate")
             subprocess.run(f"{activate_script} && python C:\\Users\\luis\\Desktop\\PAP\\PAP_Com_Server\\app.py", shell=True, text=True, check=True, executable="cmd")
+        """
 except Exception as e:
     exit(f'Erro ao rodar o servidor: {e}', 1)
