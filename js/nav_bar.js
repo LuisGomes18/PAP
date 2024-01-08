c06c901a6e9cf3b2ca30b2ee245e27307d8a514e
LuisGomes18 <luiscogomes@edu.madeira.gov.pt>
2024-01-08 11:54:59 +0000

    Uodate

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -1,15 +1,15 @@
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
+        section.scrollIntoView({ behavior: "smooth" });
     }
 }
 
-const links = document.querySelectorAll('#navbar a');
-links.forEach(link => {
-    link.addEventListener('click', (event) => {
+const links = document.querySelectorAll("#navbar a");
+links.forEach((link) => {
+    link.addEventListener("click", (event) => {
         event.preventDefault();
-        const sectionId = link.getAttribute('href').substring(1);
+        const sectionId = link.getAttribute("href").substring(1);
         scrollToSection(sectionId);
     });
 });
