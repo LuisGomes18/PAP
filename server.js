9c976bf8d2bf94b4de737ed852242b5879f65ec5
LuisGomes18 <luiscogomes@edu.madeira.gov.pt>
2024-01-08 12:16:34 +0000

    Admin

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-console.log('Iniciando o servidor...');
+console.log('> Iniciando o servidor...\n');
 const express = require('express');
 const path = require('path');
 const http = require('http');
 
 const app = express();
 const portaNormal = 3000;
-const portaAdmin = 200;
+const portaAdmin = 1024;
 
-// Middleware para permitir acesso apenas a IPs específicos
+
 const allowAdminAccess = (req, res, next) => {
-    const allowedIPs = ['127.0.0.1', '192.168.1.1']; // Substitua pelos IPs desejados
+    const allowedIPs = ['::ffff:192.168.188.12'];
     const clientIP = req.ip || req.connection.remoteAddress;
     
     if (allowedIPs.includes(clientIP)) {
-        // IP permitido, continue para a próxima rota
         next();
     } else {
-        // IP não permitido, envie uma resposta proibida (403)
         res.status(403).send('Acesso proibido');
+        console.log(`> Acesso proibido de ${clientIP}`);
     }
 };
 
@@ -37,9 +36,9 @@ const serverNormal = http.createServer(app);
 const serverAdmin = http.createServer(app);
 
 serverNormal.listen(portaNormal, () => {
-    console.log(`Servidor do site normal rodando em http://localhost:${portaNormal}`);
+    console.log(`> Servidor do site normal rodando em http://localhost:${portaNormal}`);
 });
 
 serverAdmin.listen(portaAdmin, () => {
-    console.log(`Servidor da área de administração rodando em http://localhost:${portaAdmin}/admin`);
+    console.log(`> Servidor da área de administração rodando em http://localhost:${portaAdmin}/admin\n`);
 });
