c00bb5c74ed589353c46ccfa56ae5b94c7d9e80c
LuisGomes18 <luiscogomes@edu.madeira.gov.pt>
2024-01-08 13:52:27 +0000

    Update

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const app = express()
 const portaNormal = 3000
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 100 // número máximo de requisições permitidas de um IP nesse período
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 100 // número máximo de requisições permitidas de um IP nesse período
 });
 
 app.use(limiter);
-
+app.set('trust proxy', 1);
 app.use(helmet());
 
 app.use(express.static(path.join(__dirname, 'public')))
