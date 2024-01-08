fe0246d7b5ac081ffe5f4f9b153ee69162d7222c
LuisGomes18 <luiscogomes@edu.madeira.gov.pt>
2024-01-08 13:50:19 +0000

    Update

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-console.log('> Iniciando o servidor...\n');
-const express = require('express');
-const path = require('path');
-const http = require('http');
+console.clear()
+console.log('> Iniciando o servidor...\n')
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
-const app = express();  // Instância para o servidor do site normal
 
-const portaNormal = 3000;
+const app = express()
+const portaNormal = 3000
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100 // número máximo de requisições permitidas de um IP nesse período
 });
 
-const server = http.createServer(app);
+app.use(limiter);
+
+app.use(helmet());
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+const server = http.createServer(app)
 
 server.listen(portaNormal, () => {
-    console.log(`> Servidor do site normal rodando em http://localhost:${portaNormal}`);
-});
-
+    console.log(`> Servidor do site normal rodando em http://localhost:${portaNormal}`)
+})
